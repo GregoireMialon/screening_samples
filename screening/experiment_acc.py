5a3d3581a6613aefd8ab2e6d7d8d914d4bfdf539
Gregoire Mialon <gmialon@franklin.paris.inria.fr>
2019-10-08 16:44:22 +0200

    args for exp acc

--- screening/experiment_acc.py
+++ screening/experiment_acc.py
@@ -6,32 +6,28 @@ from screening.tools import scoring_classif
 import os
 import random
 import numpy as np
+import argparse
 
-def experiment_acc():
+def experiment_acc(dataset, loss, penalty, lmbda):
 
     acc = {}
-
-    for dataset in ['mnist', 'svhn']:
-        random.seed(0)
-        np.random.seed(0)
-        if dataset == 'mnist':
-            size=60000
-        elif dataset == 'svhn':
-            size=604388
-        X, y = load_experiment(dataset=dataset, synth_params=None, size=size, redundant=0, noise=0, classification=True)
-        for loss in ['squared_hinge', 'logistic', 'safe_logistic']:
-            for penalty in ['l1', 'l2']:
-                for lmbda in [0.0001, 0.001, 0.01, 0.1, 1.0]:
-                    score = 0
-                    k = 0
-                    while k < 3:
-                        k+=1
-                        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-                        estimator = fit_estimator(X_train, y_train, loss=loss, penalty=penalty, mu=1, lmbda=lmbda, 
-                                                    intercept=False, max_iter=10000)
-                        score += scoring_classif(estimator, X_test, y_test)
-                    acc['{}_{}_{}_{}'.format(dataset, loss, penalty, lmbda)] = score / 3
-                    print('{}_{}_{}_{}'.format(dataset, loss, penalty, lmbda), ' : Done !')
+    random.seed(0)
+    np.random.seed(0)
+    if dataset == 'mnist':
+        size=60000
+    elif dataset == 'svhn':
+        size=604388
+    X, y = load_experiment(dataset=dataset, synth_params=None, size=size, redundant=0, noise=0, classification=True)
+    score = 0
+    k = 0
+    while k < 3:
+        k+=1
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        estimator = fit_estimator(X_train, y_train, loss=loss, penalty=penalty, mu=1, lmbda=lmbda, 
+                                    intercept=False, max_iter=10000)
+        score += scoring_classif(estimator, X_test, y_test)
+    acc['{}_{}_{}_{}'.format(dataset, loss, penalty, lmbda)] = score / 3
+    print('{}_{}_{}_{}'.format(dataset, loss, penalty, lmbda), ' : Done !')
 
     save_dataset_folder = os.path.join(RESULTS_PATH, 'accuracies')
     os.makedirs(save_dataset_folder, exist_ok=True)
@@ -41,5 +37,14 @@ def experiment_acc():
     return
 
 if __name__ == "__main__":
-    experiment_acc()
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', default='mnist', choices=['mnist', 'svhn'])
+    parser.add_argument('--loss', default='squared_hinge',  choices=['hinge', 'squared_hinge', 'squared','truncated_squared', 'safe_logistic', 'logistic'])
+    parser.add_argument('--penalty', default='l2', choices=['l1', 'l2'])
+    parser.add_argument('--lmbda', default=0.1, type=float)
+    args = parser.parse_args()
+
+    experiment_acc(args.dataset, args.loss, args.penalty, args.lmbda)
+
+    #for lmbda in [0.0001, 0.001, 0.01, 0.1, 1.0]:
